4344f6d3af80a0a24cd8af2451aee0d12b934fb7
François Lion <francois.lion1805@gmail.com>
2021-12-02 16:45:48 +0100

    suite fichier app

--- controller/infirmiersController.py
+++ controller/infirmiersController.py
@@ -9,18 +9,18 @@ class InfirmiersController():
     
     def delete(self,infirmier, id_infirmier):
         infirmier.delete(id_infirmier)
-        return redirect("/affichage")
+        return redirect("/infirmiers")
     
     def add(self):
-        return render_template("formulaire_infirmier.html")
+        return render_template("creation_infirmier.html")
     
     def traitementInfirmier(self, infirmier, data):
         infirmier.add(data)
-        return redirect("/infirmier")
+        return redirect("/infirmiers")
     
     def update(self, data):
         return render_template("/update_infirmier.html", data = data)
     
-    def traitementUpdate(self,continent, data):
-        continent.update(data)
-        return redirect('/affichage')
+    def traitementUpdateInfirmier(self,infirmier, data):
+        infirmier.update(data)
+        return redirect('/infirmiers')
