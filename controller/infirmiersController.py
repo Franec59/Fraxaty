5f2613788e6900a0ec26dd3af3f59cb51d85acb7
François Lion <francois.lion1805@gmail.com>
2021-12-02 16:06:53 +0100

    suite infirmier controller

--- controller/infirmiersController.py
+++ controller/infirmiersController.py
@@ -3,6 +3,24 @@ from werkzeug.utils import redirect
 
 class InfirmiersController():
     
-    def fetchInfirmiers(self, continent):
-        result = continent.fetchAll()
+    def fetchInfirmiers(self, infirmier):
+        result = infirmier.fetchAll()
         return render_template("infirmiers.html", data= result)
+    
+    def delete(self,infirmier, id_infirmier):
+        infirmier.delete(id_infirmier)
+        return redirect("/affichage")
+    
+    def add(self):
+        return render_template("formulaire_infirmier.html")
+    
+    def traitementInfirmier(self, infirmier, data):
+        infirmier.add(data)
+        return redirect("/infirmier")
+    
+    def update(self, data):
+        return render_template("/update_infirmier.html", data = data)
+    
+    def traitementUpdate(self,continent, data):
+        continent.update(data)
+        return redirect('/affichage')
